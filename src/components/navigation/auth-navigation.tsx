cb021e9a86a934188111266ed856c069cfe8fe43
sudeepmi <sudeepm0599@gmail.com>
2024-03-09 11:25:43 +0545

    bug fix

--- src/components/navigation/auth-navigation.tsx
+++ src/components/navigation/auth-navigation.tsx
@@ -1,12 +1,9 @@
 import React, { FC } from 'react'
-import Link from 'next/link'
 import Box from '@mui/material/Box'
 import { StyledButton } from '@/components/styled-button'
 import Link from 'next/link'
 
-
 const AuthNavigation: FC = () => {
-  
   return (
     <Box sx={{ '& button:first-child': { mr: 2 } }}>
       <StyledButton disableHoverEffect={true} variant="outlined">
@@ -17,14 +14,5 @@ const AuthNavigation: FC = () => {
     </Box>
   )
 }
-export default function Page() {
-  return <Link href="@/components/register/form">
-   <Box sx={{ '& button:first-child': { mr: 2 } }}>
-    <StyledButton disableHoverEffect={true} variant="outlined">
-      Sign In
-    </StyledButton>
-    <StyledButton disableHoverEffect={true}>Sign Up</StyledButton>
-   </Box>
-</Link>
-}
-// export default AuthNavigation
+
+export default AuthNavigation
